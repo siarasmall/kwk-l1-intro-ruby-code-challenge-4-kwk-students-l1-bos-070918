42d1278f64b6ce5ea16372a97db90a083c854ce3
siarasmall <siarasmall19@gmail.com>
2018-07-13 13:41:33 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,18 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+  cents % 25 % 10 % 5 = pennies
+  cents - pennies = cents_wo_pennies
+  cents_wo_pennies % 25 % 10 / 5 = nickels
+  cents_wo_pennies - nickels = cents_w_quarters_n_dimes
+  cents_w_quarters_n_dimes / 25 / 10 = dimes
+  cents_w_quarters_n_dimes - dimes / 25 = quarters
+  coins = {}
+  coins[0] = ["quarters", quarters]
+  coins[1] = ["dimes", dimes]
+  coins[2] = ["nickels", nickels]
+  coins[3] = ["pennies", pennies]
+  puts coins
 end
+
+least_coins(70)
